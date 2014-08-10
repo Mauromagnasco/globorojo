0dba26ba21f4e4e8cdad0efad6f80a4fa67667b7
Kris Jeni <jenistar90@gmail.com>
2014-08-10 22:21:57 +0700

    Initaial Commit
    Initaial Commit for Web App & Web Service

---
+++ webApp/admin/async-deleteVideo.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Globo Rojo open source application
+ *
+ *  Copyright © 2013, 2014 by Mauro Magnasco <mauro.magnasco@gmail.com>
+ *
+ *  Licensed under GNU General Public License 2.0 or later.
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-2.0+ <http://spdx.org/licenses/GPL-2.0+>
+ */
+	session_start();
+	require_once("../common/DB_Connection.php");	
+    require_once("../common/functions.php");
+
+    $result = "success";
+    $error = "";
+    $data = array();
+    
+    $videoIds = $_POST['videoIds'];
+    $sql = "delete from rb_video where rb_video in ( $videoIds )";
+    $db->query( $sql );
+    
+    $data['result'] = $result;
+    $data['error'] = $error;
+    header('Content-Type: application/json');
+    echo json_encode($data);
+?>
